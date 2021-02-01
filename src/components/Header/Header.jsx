04c71e229dad27130d693d7c70e3f63a9cb3c121
maximus676 <grillax.minsk@gmail.com>
2021-02-01 16:14:11 +0300

    test 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,6 @@ const Header = (props) => {
                     ? <div>{props.login} <button onClick={props.logout}>Log out</button></div>
                     : <NavLink to={"/Login"}>Login</NavLink>}   {/*если isAuth тру то первое если фолс то 2 */}
             </div>
-            {/*test comment*/}
         </header>);
 }
 
